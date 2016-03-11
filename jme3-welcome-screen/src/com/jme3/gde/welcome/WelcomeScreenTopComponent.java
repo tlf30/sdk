3dee57058fbb669351aeb796c61ac58d5b4c61b5
MeFisto94 <MeFisto94@users.noreply.github.com>
2016-03-12 00:07:14 +0100

    Improved Welcome Screen (Added an Icon, removed multiple entries under Windows)

--- jme3-welcome-screen/src/com/jme3/gde/welcome/WelcomeScreenTopComponent.java
+++ jme3-welcome-screen/src/com/jme3/gde/welcome/WelcomeScreenTopComponent.java
@@ -25,24 +25,25 @@ import org.openide.windows.WindowManager;
 import sun.swing.SwingUtilities2;
 
 /**
- * Top component which displays something.
+ * The Window Component itself.
+ * It will display the "Message of the Day"
  */
 @ConvertAsProperties(
     dtd = "-//com.jme3.gde.welcome//WelcomeScreen//EN",
 autostore = false)
 @TopComponent.Description(
     preferredID = "WelcomeScreenTopComponent",
-//iconBase="SET/PATH/TO/ICON/HERE", 
-persistenceType = TopComponent.PERSISTENCE_ALWAYS)
+    iconBase="com/jme3/gde/core/icons/65.png",
+    persistenceType = TopComponent.PERSISTENCE_ALWAYS)
 @TopComponent.Registration(mode = "editor", openAtStartup = true)
-@ActionID(category = "Window", id = "com.jme3.gde.welcome.WelcomeScreenTopComponent")
+@ActionID(category = "Window", id = "WelcomeScreenTopComponent")
 @ActionReference(path = "Menu/Window" /*, position = 333 */)
 @TopComponent.OpenActionRegistration(
-        displayName = "CTL_WelcomeScreenAction", /* No # since it's not part of the Bundles.properties. See http://bits.netbeans.org/dev/javadoc/org-openide-awt/org/openide/awt/ActionRegistration.html#displayName-- */
-preferredID = "WelcomeScreenTopComponent")
+        displayName = "#CTL_WelcomeScreenAction",
+        preferredID = "WelcomeScreenTopComponent")
 @Messages({
-    "CTL_WelcomeScreenAction=Info Screen",
-    "CTL_WelcomeScreenTopComponent=Info Screen",
+    "CTL_WelcomeScreenAction=Welcome Screen",
+    "CTL_WelcomeScreenTopComponent=Welcome Screen",
     "HINT_WelcomeScreenTopComponent=Shows news and information about your SDK"
 })
 public final class WelcomeScreenTopComponent extends TopComponent implements HyperlinkListener {
