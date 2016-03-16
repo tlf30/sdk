08bf5a07fa934cdf6d5e743b30bae538185394fb
MeFisto94 <MeFisto94@users.noreply.github.com>
2016-03-16 15:38:35 +0100

    One has to adjust the package after moving the file...

--- jme3-core/src/com/jme3/gde/core/CopyTemplateHandler.java
+++ jme3-core/src/com/jme3/gde/core/CopyTemplateHandler.java
@@ -29,7 +29,7 @@
  *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.jme3.gde.scenecomposer;
+package com.jme3.gde.core;
 
 import java.io.IOException;
 import java.util.ArrayList;
