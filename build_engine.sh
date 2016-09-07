b2be180edf5361be71010f02403b4a31c11d7828
MeFisto94 <MeFisto94@users.noreply.github.com>
2016-09-07 09:12:08 +0200

    Build: Remove HW Skinning Patch as it has been merged into the engine

--- build_engine.sh
+++ build_engine.sh
@@ -5,8 +5,8 @@ echo "Downloading the Engine, this may take some time"
 git clone -b v3.1 --single-branch --depth 1 http://github.com/jMonkeyEngine/jMonkeyEngine/ engine # single-branch requires git > 1.7.10, if you see an error, just leave it out.
 
 cd engine
-echo "Patching the Engine...."
-patch -s -N -p 1 < ../patches/FixHWSkinningSerialization.diff
+#echo "Patching the Engine...."
+#patch -s -N -p 1 < ../patches/FixHWSkinningSerialization.diff
 
 echo "Building the Engine and installing them to your local maven repo...."
 ./gradlew install # Depends on jarJavadoc, jarSourcecode, assemble, dist etc.
