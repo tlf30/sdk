bace2bd40ce3baa6b901e8b64b9460e67d25c4bd
MeFisto94 <MeFisto94@users.noreply.github.com>
2016-12-06 20:37:07 +0100

    Travis: Build the specific tag instead of the branch head.

--- build_engine.sh
+++ build_engine.sh
@@ -2,11 +2,11 @@
 # This Shell Script will build and use the latest jMonkeyEngine git version, so there might be some undiscovered engine bugs, watch out!
 # Also if you want to revert to releases and bintray builds, you need to uninstall them from your local maven repo...
 echo "Downloading the Engine, this may take some time"
-git clone -b v3.1 --single-branch --depth 1 http://github.com/jMonkeyEngine/jMonkeyEngine/ engine # single-branch requires git > 1.7.10, if you see an error, just leave it out.
-git checkout v3.1.0-beta2
-# tags/v3.1.0-beta2
-
+# To have the latest Commit on 3.1 use "-b v3.1". If you want a certain tag, use "-b v3.1.0-beta2"
+git clone -b v3.1.0-beta2 --single-branch --depth 1 http://github.com/jMonkeyEngine/jMonkeyEngine/ engine # single-branch requires git > 1.7.10, if you see an error, just leave it out.
 cd engine
+# git checkout tags/v3.1.0-beta2 # To use this, leave out depth and change -b to a branch.
+
 #echo "Patching the Engine...."
 #patch -s -N -p 1 < ../patches/FixHWSkinningSerialization.diff
 
